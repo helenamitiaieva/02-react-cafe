4cc56c20706a697d0fe2f2901d7cd15cf38f3619
Helena Mitiaieva <h0636641547@gmail.com>
2025-08-30 21:47:51 +0200

    update all of components and App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 import css  from "./App.module.css";
 import CafeInfo from './components/CafeInfo/CafeInfo';
 import VoteOptions from './components/VoteOptions/VoteOptions';
+import { VoteStatus } from './components/VoteStatus/VoteStatus';
+import { Notification }  from './components/Notification/Notification';
 
 const initialVotes: Votes = {good: 0, neutral: 0, bad: 0};
 
@@ -20,6 +22,11 @@ const handleVote = (type: VoteType) => {
 const resetVotes = () =>  
   setVotes(initialVotes);
 
+const totalVotes = votes.good + votes.neutral + votes.bad;
+const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
@@ -28,6 +35,15 @@ const resetVotes = () =>
       onReset={resetVotes} 
       canReset={hasAnyVotes}
       />
+{totalVotes ? (
+<VoteStatus 
+      votes={votes} 
+      totalVotes={totalVotes} 
+      positiveRate={positiveRate}
+      />
+) : (
+  <Notification/>
+)}
     </div>
   )
 }
